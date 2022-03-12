1d0266dc06e755f63ee10ad7db23ea4eee9a6161
Davide Volpe <volpe.davide.vd@gmail.com>
2022-03-12 11:06:57 +0100

    Created large examples
    Co-authored-by: Stefano Sello <ssello1@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import subprocess
 import shlex
-import datetime
+import sys
 from os import listdir
 from os.path import isfile, join
-import random
 
 import numpy as np
 
@@ -21,21 +20,16 @@ def compile():
 def run_file(inFile):
     cmd = f"{exe} {inFile}"
     print(f"Input file: `{inFile}`...")
-    start = datetime.datetime.now()
     process = subprocess.Popen(shlex.split(cmd))
     process.wait()
-    end = (datetime.datetime.now() - start).microseconds / 10
-
-    print(f"Execution took {end} ms")
+    #print(f"Execution took {end} ms")
 
 def run_tests():
     print("START all tests...")
     testFiles = [f for f in listdir(testDir) if isfile(join(testDir, f))]
-    start = datetime.datetime.now()
     for testFile in testFiles:
         run_file(join(testDir, testFile))
-    end = (datetime.datetime.now() - start).microseconds / 10
-    print(f"END tests... ({end} ms)")
+    print(f"END tests...")
 
 
 def create_distance_matrix(outFilePath, N = 2, min = 0.1, max = 5):
@@ -43,25 +37,29 @@ def create_distance_matrix(outFilePath, N = 2, min = 0.1, max = 5):
     # m = [[round(random.uniform(min, max), 1) if r<c else 0 for c in range(0, N, 1)] for r in range(0, N, 1)]
     m = np.random.rand(N,N) * (max - min) + min
     m_symm = (m + m.T)/2
+    m_symm = np.round(m_symm, 2)
     np.fill_diagonal(m_symm, 0)
-    m_symm = np.round(m_symm, 1)
 
+    float_formatter = "{:.1f}".format
+    np.set_printoptions(formatter={'float_kind':float_formatter}, threshold=sys.maxsize)
     #print(m_symm)
 
     with open(outFilePath, "w") as outFile:
         outFile.write(str(N) + '\n')
         outFile.write(' '.join(str(m_symm[i]) for i in range(0,N,1))
+            .replace("\n", '')
             .replace("[", "")
             .replace("] ", "\n")
             .replace("]", "")
-            .replace(".", "")
-            .replace(",", ""))
+            #.replace(".", "")
+            #.replace(",", "")
+        )
 
 if __name__=='__main__':
-    N = 50
+    N = 2
     file = join(testDir, f'{N}.in')
     
-    create_distance_matrix(file, N)
+    #create_distance_matrix(file, N)
 
-    compile()
-    run_file(file)
+    #compile()
+    #run_file(file)
