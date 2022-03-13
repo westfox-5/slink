654eeefa90d017d43df3fef7ef410645a00b5702
Davide Volpe <volpe.davide.vd@gmail.com>
2022-03-13 12:44:12 +0100

    Added timer debug info

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "matrix.h"
 #include "slink.h"
@@ -15,17 +16,41 @@ int main(int argc, char* const argv[]) {
        exit(1);
     }
 
-    Matrix *matrix = new ColMajorMatrix(argv[1]);
+    auto begin = std::chrono::high_resolution_clock::now();
+    
+    Matrix *matrix = new ColMajorMatrix(argv[1]);		
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = 
+      std::chrono::duration_cast<std::chrono::milliseconds>
+      (end - begin);
+
+	  std::cout << "-------------------------------" << std::endl;
+    std::cout << "Reading from file" << std::endl;
+    std::cout << "Time: " << elapsed.count() << " ms." << std::endl;
+    std::cout << std::endl;
+    
     // matrix->print();
     //std::cout << "------------" << std::endl;
 
-    Slink slink = Slink::execute(matrix);
+    begin = std::chrono::high_resolution_clock::now();
     
-    std::cout << "PI:" << std::endl;
-    printVector(slink.getPi());
-    std::cout << "------------" << std::endl;
-    std::cout << "LAMBDA:" << std::endl;
-    printVector(slink.getLambda());
+    Slink slink = Slink::execute(matrix);
+
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = 
+      std::chrono::duration_cast<std::chrono::milliseconds>
+      (end - begin);
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "Slink execution" << std::endl;
+    std::cout << "Time: " << elapsed.count() << " ms." << std::endl;
+    std::cout << std::endl;
+
+    // std::cout << "PI:" << std::endl;
+    // printVector(slink.getPi());
+    // std::cout << "------------" << std::endl;
+    // std::cout << "LAMBDA:" << std::endl;
+    // printVector(slink.getLambda());
 
     delete matrix;
 
